afcdc173b94d825e1a7db1d86b43d053ff59192d
SandrineLJ <sandrine.louisjean@gmail.com>
2025-02-12 14:12:51 +0100

    feat: dynamisation de la page catalogue TODO améliorer l'affichage en css

--- app/controllers/catalogController.js
+++ app/controllers/catalogController.js
@@ -1,7 +1,19 @@
+import dataMapper from "../data-mapper.js";
+
 const catalogController = {
 
-  renderCatalogPage(req, res) {
-    res.render("catalog");
+  async renderCatalogPage(req, res) {
+    try {
+
+      const products = await dataMapper.getAllProducts();
+
+      res.render("catalog", { products });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("une erreur s'est produite");
+    }
+    
   },
 
   renderProductPage(req, res) {
